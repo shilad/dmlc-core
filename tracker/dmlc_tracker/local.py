f6151a67205d3fa0e7640672011051be01667c95
Shilad Sen <Shilad.Sen@target.com>
2016-05-29 21:05:23 -0500

    Getting good results. Ready to test on larger datasets.

--- tracker/dmlc_tracker/local.py
+++ tracker/dmlc_tracker/local.py
@@ -80,4 +80,4 @@ def submit(args):
 
     # call submit, with nslave, the commands to run each job and submit function
     tracker.submit(args.num_workers, args.num_servers, fun_submit=mthread_submit,
-                   pscmd=(' '.join(args.command)))
+                   pscmd=(' '.join(args.command)), hostIP='localhost')
